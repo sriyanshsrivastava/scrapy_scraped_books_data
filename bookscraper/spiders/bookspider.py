29b116f62c3d20dafcda2d834b2bc88e60bf0862
Sriyansh Srivastava <sriyanshsrivastava99@gmail.com>
2025-05-28 17:01:29 +0530

    added book page scrap code

--- bookscraper/spiders/bookspider.py
+++ bookscraper/spiders/bookspider.py
@@ -9,11 +9,18 @@ class BookspiderSpider(scrapy.Spider):
     def parse(self, response):
         books=response.css("article.product_pod")
         for book in books:
-            yield{
-                "name":book.css("h3 a::text").get(),
-                "price":book.css(".procuct_price .price_color::text").get(),
-                "url":book.css("h3 a").attrib["href"]
-            }
+            relative_url=book.css("h3 a::attr(href)").get()
+            if "catalogue/" in relative_url:
+                book_url="https://books.toscrape.com/" + relative_url
+            else:
+                book_url="https://books.toscrape.com/catalogue/" + relative_url
+            yield response.follow(book_url, callback = self.parse_book_page)
+
+            # yield{
+            #     "name":book.css("h3 a::text").get(),
+            #     "price":book.css(".procuct_price .price_color::text").get(),
+            #     "url":book.css("h3 a").attrib["href"]
+            # }
         
         next_page=response.css("li.next a::attr(href)").get()
 
@@ -24,3 +31,20 @@ class BookspiderSpider(scrapy.Spider):
                 next_page_url="https://books.toscrape.com/catalogue/" + next_page
 
             yield response.follow(next_page_url, callback=self.parse)
+
+
+    def parse_book_page(self,response):
+        table_rows=response.css("table tr")
+        yield{
+            "url":response.url,
+            "title":response.css(".product_main h1::text").get(),
+            "product type":table_rows[1].css("td::text").get(),
+            "price_excl_tax":table_rows[2].css("td::text").get(),
+            "price_incl_tax":table_rows[3].css("td::text").get(),
+            "tax":table_rows[4].css("td::text").get(),
+            "availability":table_rows[5].css("td::text").get(),
+            "num_reviews":table_rows[6].css("td::text").get(),
+            "stars":response.css("p.star-rating::attr(class)").get().split()[-1],
+            "category":response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
+            "price":response.css("p.price_color::text").get(),
+        }
